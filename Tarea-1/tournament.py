798896401f0c773cce0988cf7dde2b94857a4ab9
PinkFloyd92 <ascacere92@gmail.com>
2015-12-15 17:54:49 -0500

    filling teams

--- Tarea-1/tournament.py
+++ Tarea-1/tournament.py
@@ -9,7 +9,7 @@ class Tournament:
         self.filename = filename
         for team_name in self.team_names:
             teams.append(Team(team_name))
-        return
+    
     def fill_team(self,stats):
         '''stats[0]->teamName,stats[1]->win=1,lose=-1,tie=0
         ,stats[2]->goals,stats[3]->received_goals'''
@@ -28,8 +28,9 @@ class Tournament:
     def load_teams(self):
         #indexes where the team was found.
         index = [0,0]
-        team_1 = []
-        team_2 = []
+        team_temp = [] # [[equipo1,indice inicial], [equipo2, indice inicial]]
+        marcador = [] #[score_team1, score_team2]
+        stats = [] #stats from 2 teams
         indice_contador = 0
         f = open(self.filename, 'r')
         for line in f:
@@ -37,7 +38,33 @@ class Tournament:
             for name in self.team_names:
                 indice = line.find(name)
                 if  indice != -1:
-                    team_1[indice_contador] = [name,indice]
+                    team_temp[indice_contador] = [name,indice]
                     indice_contador= indice_contador + 1
             indice_contador = 0
-                
+            marcador = [int(s) for s in line.split() if s.isdigit()]
+            if len(marcador) !=2:
+                next(f) #skip line
+                index = [0,0]
+                team_temp = []
+                stats = []
+            else: #if we get the correct score.
+                if(team_temp[0][1] >team_temp[1][1]):
+                    if marcador[0] > marcador[1]:
+                        stats[0] =[team_temp[1],1,marcador[0],marcador[1]]
+                        stats[1] = [team_temp[0],-1,marcador[1],marcador[0]]
+                    if marcador[0] == marcador[1]:
+                        stats[0] =[team_temp[1],0,marcador[0],marcador[1]]
+                        stats[1] = [team_temp[0],0,marcador[1],marcador[0]]
+                    else:
+                        stats[0] = [team_temp[1],-1,marcador[0],marcador[1]]
+                        stats[1] = [team_temp[0],1,marcador[1],marcador[0]]
+                else:
+                    if marcador[1] > marcador[0]:
+                        stats[0] =[team_temp[1],1,marcador[0],marcador[1]]
+                        stats[1] = [team_temp[0],-1,marcador[1],marcador[0]]
+                    if marcador[0] == marcador[1]:
+                        stats[0] =[team_temp[1],0,marcador[0],marcador[1]]
+                        stats[1] = [team_temp[0],0,marcador[1],marcador[0]]
+                    else:
+                        stats[0] = [team_temp[1],-1,marcador[0],marcador[1]]
+                        stats[1] = [team_temp[0],1,marcador[1],marcador[0]]
