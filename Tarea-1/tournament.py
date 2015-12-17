e104f4a9827f878985e6129a46f2d324e5cf4b5a
PinkFloyd92 <ascacere92@gmail.com>
2015-12-17 01:32:30 -0500

    indentacion

--- Tarea-1/tournament.py
+++ Tarea-1/tournament.py
@@ -194,15 +194,15 @@ def insertion_sort(a):
         a[j+1] = key
         
 def quickSort(alist):
-   quickSortHelper(alist,0,len(alist)-1)
+    quickSortHelper(alist,0,len(alist)-1)
 
 def quickSortHelper(alist,first,last):
-   if first<last:
+    if first<last:
 
-       splitpoint = partition(alist,first,last)
+        splitpoint = partition(alist,first,last)
 
-       quickSortHelper(alist,first,splitpoint-1)
-       quickSortHelper(alist,splitpoint+1,last)
+        quickSortHelper(alist,first,splitpoint-1)
+        quickSortHelper(alist,splitpoint+1,last)
 
 
 def partition(alist,first,last):
